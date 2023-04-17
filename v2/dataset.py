455dd8413e51a69711e1b1dfc926dda7bdba5853
guk98 <guk987@naver.com>
2023-04-17 16:41:08 +0900

    add efficientent model

--- v2/dataset.py
+++ v2/dataset.py
@@ -195,7 +195,7 @@ class MaskBaseDataset(Dataset):
         age_label = self.get_age_label(index)
         multi_class_label = self.encode_multi_class(mask_label, gender_label, age_label)
 
-        image_transform = self.transform(image)
+        image_transform = self.transform(image=image)
         return image_transform, multi_class_label, age_num_label
 
     def __len__(self):
