04c485472690e1662b24c508e0f9e8542276c013
junha-lee <junha4304@gmail.com>
2023-04-17 08:04:25 +0000

    add timm efficientbase

--- v2/model.py
+++ v2/model.py
@@ -37,9 +37,8 @@ class BaseModel(nn.Module):
 class EfficientBase(nn.Module):
     def __init__(self, num_classes=18):
         super().__init__()
-        from torchvision.models import efficientnet_b0
-        self.backbone = efficientnet_b0(pretrained=True)
-        self.backbone.classifier[1]= nn.Linear(1000,num_classes)
+        import timm
+        self.backbone = timm.create_model('efficientnet_b0', pretrained=True, num_classes=num_classes)
 
     def forward(self, x):
         out = self.backbone(x)
