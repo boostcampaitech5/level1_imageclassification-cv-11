73f1cc8fb80f11b87e363cc82b4dbbf1c42fd50c
jiyoulee <jiyouleehome@gmail.com>
2023-04-17 08:09:29 +0000

    revert: revert num_workers from 3 back to 2

--- v2/train.py
+++ v2/train.py
@@ -132,7 +132,7 @@ def train(data_dir, model_dir, args):
         train_loader = DataLoader(
             train_sets[k],
             batch_size=args.batch_size,
-            num_workers=multiprocessing.cpu_count() // 3,
+            num_workers=multiprocessing.cpu_count() // 2,
             shuffle=True,
             pin_memory=use_cuda,
             drop_last=True,
@@ -141,7 +141,7 @@ def train(data_dir, model_dir, args):
         val_loader = DataLoader(
             val_sets[k],
             batch_size=args.valid_batch_size,
-            num_workers=multiprocessing.cpu_count() // 3,
+            num_workers=multiprocessing.cpu_count() // 2,
             shuffle=False,
             pin_memory=use_cuda,
             drop_last=True,
