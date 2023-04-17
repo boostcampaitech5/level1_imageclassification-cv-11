f3dc4671c905b1fd28d1f21758dcb5d518f4de84
jiyoulee <jiyouleehome@gmail.com>
2023-04-17 12:33:21 +0000

    feat: implement resnet18 model

--- v2/model.py
+++ v2/model.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 import torch.nn.functional as F
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -24,6 +25,11 @@ import timm
 <<<<<<< HEAD
 =======
 =======
+=======
+import torchvision.models as models
+
+classification_models = models.list_models(module=models)
+>>>>>>> 5f29eb0... feat: implement resnet18 model
 
 >>>>>>> c1eb4f8... add baselinev2
 =======
@@ -332,6 +338,17 @@ class EfficientBase(nn.Module):
        
         return out
     
+class ResNet18(nn.Module):
+    def __init__(self, num_classes=18):
+        super().__init__()
+        
+        self.net = models.get_model('ResNet18', weights='DEFAULT')
+
+    def forward(self, x):
+        out = self.net(x)
+       
+        return out
+    
 # class MultiLabelModel(nn.module):
 #     def __init__(self):
 #         super().__init__()
@@ -364,6 +381,7 @@ class EfficientBase(nn.Module):
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 #         return out_mask, out_gender, out_age_class, out_age_num
 =======
         return out_mask, out_gender, out_age_class, out_age_num
@@ -392,3 +410,6 @@ class EfficientBase(nn.Module):
 =======
 #         return out_mask, out_gender, out_age_class, out_age_num
 >>>>>>> 686cc95... add efficientent model
+=======
+#         return out_mask, out_gender, out_age_class, out_age_num
+>>>>>>> 5f29eb0... feat: implement resnet18 model
