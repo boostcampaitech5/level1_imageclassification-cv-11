0e9be4b6503a1f4a457065c682bbdbe9bdbe95bf
jiyoulee <jiyouleehome@gmail.com>
2023-04-17 13:04:01 +0000

    fix: allow different model parameters depending on model type

--- v2/train.py
+++ v2/train.py
@@ -203,6 +203,7 @@ def train(data_dir, model_dir, args):
     train_loader, val_loader = fold(i)
     
     # model_module = getattr(import_module("model"), args.model)  # default: BaseModel
+<<<<<<< HEAD
     model = model_module().to(device)
 <<<<<<< HEAD
 =======
@@ -259,7 +260,16 @@ def train(data_dir, model_dir, args):
     # model_module = getattr(import_module("model"), args.model)  # default: BaseModel
     model = model_module(num_classes=num_classes).to(device)
 >>>>>>> b41054b... split
+<<<<<<< HEAD
 >>>>>>> 9c1f8f7... split
+=======
+=======
+    if ('EfficientBase' == args.model):
+        model = model_module(num_classes=num_classes).to(device)
+    else:
+        model = model_module().to(device)
+>>>>>>> 7e1cbeb... fix: allow different model parameters depending on model type
+>>>>>>> 5d68c6d... fix: allow different model parameters depending on model type
     model = torch.nn.DataParallel(model)
 
     # -- loss & metric
