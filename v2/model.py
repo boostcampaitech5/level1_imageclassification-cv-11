1dd33c90a2a62a6494e8888b318903e9ae5de1cf
jiyoulee <jiyouleehome@gmail.com>
2023-04-18 05:24:51 +0000

    feat: implement age regression information into training for single output model

--- v2/model.py
+++ v2/model.py
@@ -39,7 +39,7 @@ class BaseModel(nn.Module):
 
 # Custom Model Template
 class EfficientBase(nn.Module):
-    def __init__(self, num_classes=18):
+    def __init__(self, num_classes=1000):
         super().__init__()
         self.net = timm.create_model('efficientnet_b0', pretrained=True, num_classes=num_classes)
 
@@ -50,7 +50,7 @@ class EfficientBase(nn.Module):
     
 <<<<<<< HEAD
 class ResNet18(nn.Module):
-    def __init__(self, num_classes=18):
+    def __init__(self, num_classes=1000):
         super().__init__()
         
         self.net = timm.create_model('resnet18', pretrained=True, num_classes=num_classes)
@@ -61,7 +61,7 @@ class ResNet18(nn.Module):
         return out
 
 class ResNet34(nn.Module):
-    def __init__(self, num_classes=18):
+    def __init__(self, num_classes=1000):
         super().__init__()
 
         self.net = timm.create_model('resnet34', pretrained=True, num_classes=num_classes)
@@ -72,7 +72,7 @@ class ResNet34(nn.Module):
         return out
 
 class EfficientNetB1(nn.Module):
-    def __init__(self, num_classes=18):
+    def __init__(self, num_classes=1000):
         super().__init__()
 
         self.net = timm.create_model('efficientnet_b1', pretrained=True, num_classes=num_classes)
@@ -83,7 +83,7 @@ class EfficientNetB1(nn.Module):
         return out
 
 class EfficientNetB2(nn.Module):
-    def __init__(self, num_classes=18):
+    def __init__(self, num_classes=1000):
         super().__init__()
 
         self.net = timm.create_model('efficientnet_b2', pretrained=True, num_classes=num_classes)
@@ -94,7 +94,7 @@ class EfficientNetB2(nn.Module):
         return out
 
 class ViTTiny_Patch16_384(nn.Module):
-    def __init__(self, num_classes=18):
+    def __init__(self, num_classes=1000):
         super().__init__()
 
         self.net = timm.create_model('vit_tiny_patch16_384', pretrained=True, num_classes=num_classes)
@@ -105,7 +105,7 @@ class ViTTiny_Patch16_384(nn.Module):
         return out
 
 class ViTSmall_Patch16_384(nn.Module):
-    def __init__(self, num_classes=18):
+    def __init__(self, num_classes=1000):
         super().__init__()
 
         self.net = timm.create_model('vit_small_patch16_384', pretrained=True, num_classes=num_classes)
@@ -115,6 +115,7 @@ class ViTSmall_Patch16_384(nn.Module):
 
         return out
 
+<<<<<<< HEAD
 
 # class MultiLabelModel(nn.module):
 #     def __init__(self):
@@ -132,6 +133,23 @@ class ViTSmall_Patch16_384(nn.Module):
 #         3. 모델의 output_dimension 은 num_classes 로 설정해주세요.
 #         """
 =======
+=======
+class SingleOutputModel(nn.Module):
+    def __init__(self, in_features=1000, model=EfficientBase()):
+        super().__init__()
+
+        self.net = model
+        self.branch_class = nn.Linear(in_features=in_features, out_features=18)
+        self.branch_age_val = nn.Linear(in_features=in_features, out_features=1)
+
+    def forward(self, x):
+        out = self.net(x)
+        out_class = self.branch_class(out)
+        out_age_num = self.branch_age_val(out)
+
+        return out_class, out_age_num
+
+>>>>>>> 6c7fa68... feat: implement age regression information into training for single output model
 class MultiOutputModel(nn.Module):
     def __init__(self, in_features=1000, model=EfficientBase()):
         super().__init__()
