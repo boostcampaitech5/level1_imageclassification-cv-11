652058c8023c9b7b1b7cfe9a3944fac621217c21
chaewon Lee <lcwon9@gmail.com>
2023-04-20 02:01:31 +0000

    .

--- v2/train.py
+++ v2/train.py
@@ -15,7 +15,7 @@ from torch.optim.lr_scheduler import StepLR
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 
-from dataset_seg_2 import MaskBaseDataset
+from dataset import MaskBaseDataset
 from loss import create_criterion
 import augmentation
 import model as Models
