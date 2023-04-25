79d81c6158fe872b607f841e02e2fa7e9c672fcc
chaewon Lee <lcwon9@gmail.com>
2023-04-25 08:10:47 +0000

    train train_multi

--- train_multi.py
+++ train_multi.py
@@ -19,7 +19,7 @@ from dataset import MaskBaseDataset
 from loss import create_criterion
 import augmentation
 import model as Models
-import wandb
+# import wandb
 from sklearn.metrics import f1_score, accuracy_score
 
 
@@ -35,39 +35,7 @@ def seed_everything(seed):
 def get_lr(optimizer):
     for param_group in optimizer.param_groups:
         return param_group['lr']
-    
-def get_k(optimizer):
-    for param_group in optimizer.param_groups:
-        return param_group['fold']
 
-def grid_image(np_images, gts, preds, n=16, shuffle=False):
-    batch_size = np_images.shape[0]
-    assert n <= batch_size
-
-    choices = random.choices(range(batch_size), k=n) if shuffle else list(range(n))
-    figure = plt.figure(figsize=(12, 18 + 2))  # cautions: hardcoded, 이미지 크기에 따라 figsize 를 조정해야 할 수 있습니다. T.T
-    plt.subplots_adjust(top=0.8)  # cautions: hardcoded, 이미지 크기에 따라 top 를 조정해야 할 수 있습니다. T.T
-    n_grid = int(np.ceil(n ** 0.5))
-    tasks = ["mask", "gender", "age"]
-    for idx, choice in enumerate(choices):
-        gt = gts[choice].item()
-        pred = preds[choice].item()
-        image = np_images[choice]
-        gt_decoded_labels = MaskBaseDataset.decode_multi_class(gt)
-        pred_decoded_labels = MaskBaseDataset.decode_multi_class(pred)
-        title = "\n".join([
-            f"{task} - gt: {gt_label}, pred: {pred_label}"
-            for gt_label, pred_label, task
-            in zip(gt_decoded_labels, pred_decoded_labels, tasks)
-        ])
-
-        plt.subplot(n_grid, n_grid, idx + 1, title=title)
-        plt.xticks([])
-        plt.yticks([])
-        plt.grid(False)
-        plt.imshow(image, cmap=plt.cm.binary)
-
-    return figure
 
 def increment_path(path, exist_ok=False):
     """ Automatically increment path, i.e. runs/exp --> runs/exp0, runs/exp1 etc.
@@ -94,13 +62,9 @@ def train(data_dir, model_dir, save_dir, args):
     device = torch.device("cuda" if use_cuda else "cpu")
 
     # -- dataset
-    dataset_module = getattr(import_module("dataset"), args.dataset)  # default: MaskBaseDataset
-    dataset = dataset_module(
-        data_dir=data_dir,
-    )
     
-    # mean, std = (0.56019265, 0.52410305, 0.50145299), (0.23308824, 0.24294489, 0.2456003)
     
+   
     train_dataset = MaskBaseDataset(
         data_dir=data_dir,
         segmentation = args.seg
@@ -111,7 +75,7 @@ def train(data_dir, model_dir, save_dir, args):
         segmentation = args.seg
     )
     
-    num_classes = dataset.num_classes  # 18
+   
 
     # -- augmentation
     transform = augmentation.get_transforms()  # default: BaseAugmentation
@@ -170,11 +134,6 @@ def train(data_dir, model_dir, save_dir, args):
     )
     scheduler = StepLR(optimizer, args.lr_decay_step, gamma=0.5)
 
-    # -- logging
-    logger = SummaryWriter(log_dir=save_dir)
-    with open(os.path.join(save_dir, 'config.json'), 'w', encoding='utf-8') as f:
-        json.dump(vars(args), f, ensure_ascii=False, indent=4)
-
     best_val_f1 = 0
     best_val_loss = np.inf
     for epoch in range(args.epochs):
@@ -220,9 +179,7 @@ def train(data_dir, model_dir, save_dir, args):
             optimizer.step()
 
             loss_value += loss.item()
-            # mask_matches += (mask_outs.argmax(1).detach().cpu().numpy().tolist() == mask_labels).sum().item()
-            # gender_matches += (gender_outs.argmax(1).detach().cpu().numpy().tolist() == gender_labels).sum().item()
-            # age_matches += (age_outs.argmax(1).detach().cpu().numpy().tolist() == age_labels).sum().item()
+          
             pred1 = mask_outs.argmax(1).detach().cpu().numpy()
             pred2 = gender_outs.argmax(1).detach().cpu().numpy()
             pred3 = age_outs.argmax(1).detach().cpu().numpy()
@@ -246,9 +203,7 @@ def train(data_dir, model_dir, save_dir, args):
                     f"Epoch[{epoch}/{args.epochs}]({idx + 1}/{len(train_loader)}) || "
                     f"training loss {train_loss:4.4} || training accuracy {train_acc:4.2%} || lr {current_lr}"
                 )
-                logger.add_scalar("Train/loss", train_loss, epoch * len(train_loader) + idx)
-                logger.add_scalar("Train/accuracy", train_acc, epoch * len(train_loader) + idx)
-
+                
                 loss_value = 0
                 matches = 0
 
@@ -288,7 +243,6 @@ def train(data_dir, model_dir, save_dir, args):
                                 
                 mask_outs, gender_outs, age_outs, age_num_outs = model(inputs)
                 
-                # preds = torch.argmax(outs, dim=-1)
                 loss1 = criterion_mask(mask_outs, mask_labels)
                 loss2 = criterion_gender(gender_outs, gender_labels)
                 loss3 = criterion_age(age_outs, age_labels)
@@ -302,9 +256,7 @@ def train(data_dir, model_dir, save_dir, args):
                 val_loss2.append(loss2.item())
                 val_loss3.append(loss3.item())
                 val_loss4.append(loss4.item())
-
-
-                
+             
                 pred1 = mask_outs.argmax(1).detach().cpu().numpy()
                 pred2 = gender_outs.argmax(1).detach().cpu().numpy()
                 pred3 = age_outs.argmax(1).detach().cpu().numpy()
@@ -315,14 +267,6 @@ def train(data_dir, model_dir, save_dir, args):
                 
                 val_category_preds += (pred1*6+pred2*3+pred3).tolist()
                 val_category_trues += labels.detach().cpu().numpy().tolist()
-
-                if figure is None:
-                    preds = torch.Tensor(pred1*6+pred2*3+pred3)
-                    inputs_np = torch.clone(inputs).detach().cpu().permute(0, 2, 3, 1).numpy()
-                    inputs_np = dataset_module.denormalize_image(inputs_np, dataset.mean, dataset.std)
-                    figure = grid_image(
-                        inputs_np, labels, preds, n=16, shuffle=args.dataset != "MaskSplitByProfileDataset"
-                    )
 
             val_loss = np.sum(val_loss_items) / len(val_loader)
             val_acc = np.mean(val_acc_items)
@@ -343,11 +287,9 @@ def train(data_dir, model_dir, save_dir, args):
             
             _val_f1 = f1_score(val_category_trues,val_category_preds, average='macro')
 
-            wandb.log({'Train loss':_train_loss,'Train F1':_train_f1,'Train Acc':_train_accuracy,'Train loss 1' : _train_loss1,'Train loss 2' : _train_loss2,'Train loss 3' : _train_loss3,'Train loss 4':_train_loss4,'Train loss 123':_train_loss-_train_loss4,
-                   'Val loss':val_loss,'Val F1':_val_f1, 'Val Acc':val_acc,'Val loss 1' : _val_loss1,'Val loss 2' : _val_loss2,'Val loss 3' : _val_loss3,'Val loss 4':_val_loss4,'Val loss 123':val_loss-_val_loss4,'Val best f1':max(_val_f1, best_val_f1)})      ## logging wandb
-            
-            
-            
+            # wandb.log({'Train loss':_train_loss,'Train F1':_train_f1,'Train Acc':_train_accuracy,'Train loss 1' : _train_loss1,'Train loss 2' : _train_loss2,'Train loss 3' : _train_loss3,'Train loss 4':_train_loss4,'Train loss 123':_train_loss-_train_loss4,
+            #        'Val loss':val_loss,'Val F1':_val_f1, 'Val Acc':val_acc,'Val loss 1' : _val_loss1,'Val loss 2' : _val_loss2,'Val loss 3' : _val_loss3,'Val loss 4':_val_loss4,'Val loss 123':val_loss-_val_loss4,'Val best f1':max(_val_f1, best_val_f1)})      ## logging wandb
+          
             if _val_f1 > best_val_f1:
                 print(f"New best model for val f1 : {_val_f1:4.2%}! saving the best model..")
                 torch.save(model.module.state_dict(), f"{save_dir}/best.pth")
@@ -357,9 +299,6 @@ def train(data_dir, model_dir, save_dir, args):
                 f"[Val] f1 : {_val_f1:4.2%}, loss: {val_loss:4.2} || "
                 f"best f1 : {best_val_f1:4.2%}, best loss: {best_val_loss:4.2}"
             )
-            logger.add_scalar("Val/loss", val_loss, epoch)
-            logger.add_scalar("Val/f1", _val_f1, epoch)
-            logger.add_figure("results", figure, epoch)
             print()
 
 
@@ -398,26 +337,26 @@ if __name__ == '__main__':
     model_dir = args.model_dir
     save_dir = increment_path(os.path.join(model_dir, args.name))
     
-    config = {'save_dir' : save_dir,
-            'use_age': args.use_age,
-            'multi':True, 
-            'seg':args.seg,
-            'mislabel': args.mislabel,
-            'model':args.model,
-            'augmentation':args.augmentation,
-            'batch_size':args.batch_size,
-            'criterion':args.criterion,
-            'epoch':args.epochs,
-            'fold':args.fold,
-            'lr':args.lr,
-            'lr_decay_step':args.lr_decay_step,
-            'optimizer':args.optimizer,
-            'resize':str(args.resize[0])+'X'+str(args.resize[1]),
-            'seed':args.seed}
+    # config = {'save_dir' : save_dir,
+    #         'use_age': args.use_age,
+    #         'multi':True, 
+    #         'seg':args.seg,
+    #         'mislabel': args.mislabel,
+    #         'model':args.model,
+    #         'augmentation':args.augmentation,
+    #         'batch_size':args.batch_size,
+    #         'criterion':args.criterion,
+    #         'epoch':args.epochs,
+    #         'fold':args.fold,
+    #         'lr':args.lr,
+    #         'lr_decay_step':args.lr_decay_step,
+    #         'optimizer':args.optimizer,
+    #         'resize':str(args.resize[0])+'X'+str(args.resize[1]),
+    #         'seed':args.seed}
     
-    dir = save_dir.split('/')[-1]
+    # dir = save_dir.split('/')[-1]
     
-    # # wandb.init(project="test",name=f'use_age:{args.use_age}-multi:0-seg:{args.seg}-mislabel:{args.mislabel}-model:{args.model}-augmentation:{args.augmentation}-batch_size:{args.batch_size}-criterion:{args.criterion}-epoch:{args.epochs}-fold:{args.fold}-lr:{args.lr}-lr_decay_step:{args.lr_decay_step}-optimizer:{args.optimizer}-resize:{args.resize[0]}X{args.resize[1]}-seed:{args.seed}')
-    wandb.init(project='image-classification-challenge-v2',name=f'use_age:{args.use_age}-multi:{True}-seg:{args.seg}-mislabel:{args.mislabel}-model:{args.model}-exp:{dir}',config= config)
+
+    # wandb.init(project='image-classification-challenge-v2',name=f'use_age:{args.use_age}-multi:{True}-seg:{args.seg}-mislabel:{args.mislabel}-model:{args.model}-exp:{dir}',config= config)
 
     train(data_dir, model_dir,save_dir, args)
