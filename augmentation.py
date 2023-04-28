9769f9be01a99bd7467275c850211914f9026377
jiyoulee <jiyouleehome@gmail.com>
2023-04-28 17:46:33 +0900

    style: remove unnecessary comments from augmentation.py

--- augmentation.py
+++ augmentation.py
@@ -3,18 +3,6 @@ from albumentations.pytorch import ToTensorV2
 
 
 def get_transforms(need=('train', 'val'), img_size=(512, 384), mean=(0.548, 0.504, 0.479), std=(0.237, 0.247, 0.246)):
-    """
-    train 혹은 validation의 augmentation 함수를 정의합니다. train은 데이터에 많은 변형을 주어야하지만, validation에는 최소한의 전처리만 주어져야합니다.
-    
-    Args:
-        need: 'train', 혹은 'val' 혹은 둘 다에 대한 augmentation 함수를 얻을 건지에 대한 옵션입니다.
-        img_size: Augmentation 이후 얻을 이미지 사이즈입니다.
-        mean: 이미지를 Normalize할 때 사용될 RGB 평균값입니다.
-        std: 이미지를 Normalize할 때 사용될 RGB 표준편차입니다.
-
-    Returns:
-        transformations: Augmentation 함수들이 저장된 dictionary 입니다. transformations['train']은 train 데이터에 대한 augmentation 함수가 있습니다.
-    """
     transformations = {}
     if 'train' in need:
         transformations['train'] = Compose([
